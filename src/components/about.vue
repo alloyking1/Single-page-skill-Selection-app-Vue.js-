3e0369f4d3863a17543b3be564acaa90874a90a4
alloyking1 <alloyking1@gmail.com>
2018-04-13 13:28:12 +0100

    pushing vue skill app

---
+++ src/components/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About AlloyKing1</h1>
+    <p>This is a demo app to demonstrate some of the new cool stuffs i learnt using vue.js..</p>
+  </div>
+</template>
